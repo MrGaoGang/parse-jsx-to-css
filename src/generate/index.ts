5f3e17a2c47f081114c8ece9d451d34cd0ccccaa
alexganggao <alexganggao@tencent.com>
2021-03-01 20:00:57 +0800

    fix: css prettier plugins

--- src/generate/index.ts
+++ src/generate/index.ts
@@ -1,9 +1,11 @@
 import { BaseConfig, ItemClassName } from "../types";
 import * as prettier from "prettier";
+import cssPlugins from 'prettier/parser-postcss'
 export default function (code: string, options: BaseConfig): Promise<string> {
   return new Promise<string>((resolve, reject) => {
     code = prettier.format(code, {
       parser: options.outType === "sass" ? "scss" : options.outType || "less",
+      plugins:[cssPlugins]
     });
     resolve(code);
   });
